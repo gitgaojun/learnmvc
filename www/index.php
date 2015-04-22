13b1bebf2f0f4fe9fd629c2c7bbd2282839ea01b
gaojun610 <gaojun610gj@gmail.com>
2015-04-22 18:22:24 +0800

    add htaccess file

--- www/index.php
+++ www/index.php
@@ -4,14 +4,14 @@
 //文件的路径
 define("APPPATH", rtrim(__DIR__,"/")."/");
 //控制器的路径
-define("CDIR", rtrim(APPPATH."../application/controllers","/")."/");
+define("CPATH", rtrim(APPPATH."../application/controllers","/")."/");
 //模版的路径
-define("MDIR", rtrim(APPPATH."../application/models","/")."/");
+define("MPATH", rtrim(APPPATH."../application/models","/")."/");
 //视图的路径
-define("VDIR", rtrim(APPPATH."define", "/")."/");
+define("VPATH", rtrim(APPPATH."define", "/")."/");
 //系统路径
-define("SYSDIR", rtrim(APPPATH."../system", "/")."/");
+define("SYSPATH", rtrim(APPPATH."../system", "/")."/");
 
 
 
-require_once(SYSDIR."core/Sparrow.php");
+require_once(SYSPATH."core/Sparrow.php");
