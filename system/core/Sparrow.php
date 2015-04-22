28bfe2f07548ad37c34d1d686be77f9a4b842e8e
gaojun610 <gaojun610gj@gmail.com>
2015-04-22 11:23:39 +0800

    建立核心加载文件

---
+++ system/core/Sparrow.php
@@ -0,0 +1,16 @@
+<?php
+defined("APPPATH") OR exit("No direct script access allowed");
+/**
+ * Created by PhpStorm.
+ * User: jun90610@gmail.com
+ * Date: 2015/4/22
+ * Time: 11:14
+ */
+
+    class Sparrow
+    {
+        
+
+
+
+    }
