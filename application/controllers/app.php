d787fb97eb33f645ee7fa09a5f9195ded1565c87
gaojun610 <gaojun610gj@gmail.com>
2015-04-22 18:21:23 +0800

    app default file

--- application/controllers/app.php
+++ application/controllers/app.php
@@ -1,7 +1,28 @@
 <?php
+defined("APPPATH") or exit("No direct script access allowed");
 /**
  * Created by PhpStorm.
  * User: jun90610@gmail.com
  * Date: 2015/4/22
  * Time: 10:29
  */
+
+    class app extends SR_Controller
+    {
+
+        function __construct()
+        {
+            parent::__construct();
+        }
+
+        public function index()
+        {
+
+            
+            $data['title'] = "Sparrow-shop";
+
+            $this->vars($data);
+            $this->view("app");
+        }
+
+    }
