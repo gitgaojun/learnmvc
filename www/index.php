7592d9240face1b29d477fad9cb57c1aff6720f1
gaojun610 <gaojun610gj@gmail.com>
2015-04-22 11:24:45 +0800

    修改入口文件

--- www/index.php
+++ www/index.php
@@ -1,6 +1,17 @@
 <?php 
 
-echo 3;
+
+//文件的路径
+define("APPPATH", rtrim(__DIR__,"/")."/");
+//控制器的路径
+define("CDIR", rtrim(APPPATH."../application/controllers","/")."/");
+//模版的路径
+define("MDIR", rtrim(APPPATH."../application/models","/")."/");
+//视图的路径
+define("VDIR", rtrim(APPPATH."define", "/")."/");
+//系统路径
+define("SYSDIR", rtrim(APPPATH."../system", "/")."/");
 
 
 
+require_once(SYSDIR."core/Sparrow.php");
